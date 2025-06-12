f579666e69c2f83090bb9d01f0a2bb0cbfe2066b
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-12 22:14:09 +0200

    null, default and blank

--- sales_app/models.py
+++ sales_app/models.py
@@ -4,5 +4,5 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     newletter_abo = models.BooleanField(default=False)
-    # email_adress = models.EmailField()
-    # account = models.FloatField()
+    email_adress = models.CharField(max_length=30, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)
