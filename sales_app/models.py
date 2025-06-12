c4a072a616dcd150bcfc04a53eb24a243484c669
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-12 21:41:38 +0200

    added first customers

--- sales_app/models.py
+++ sales_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-class Person(models.Model):
+class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    # newletter_abo = models.BooleanField()
+    # email_adress = models.EmailField()
+    # account = models.FloatField()
