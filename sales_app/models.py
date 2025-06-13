b4e0e218fc826b2e380bd0eaf2e9784f0007155b
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-13 10:50:34 +0200

    one to many and many to many

--- sales_app/models.py
+++ sales_app/models.py
@@ -6,3 +6,13 @@ class Customer(models.Model):
     newletter_abo = models.BooleanField(default=False)
     email_adress = models.CharField(max_length=30, blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField(default=9.99)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    # pass
+
