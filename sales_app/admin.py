59f1bda5ad2c59833682739b560a2dd80e2067de
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-06-17 22:17:26 +0200

    admin superuser

--- sales_app/admin.py
+++ sales_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer, Bill, Product, ProductType, Order
+
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter=['first_name', 'last_name']
+    list_display=['first_name', 'last_name']
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Bill)
+admin.site.register(Product)
+admin.site.register(ProductType)
+admin.site.register(Order)
+
